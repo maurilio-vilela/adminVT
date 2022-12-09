80a30c9eaa16d17bd9fe061e60f453cb59a4fedc
Maurilio Vilela <mauriliovilela20@gmail.com>
2022-12-09 17:37:48 -0300

    Criado componente Home

---
+++ src/components/Home.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import Button from "./Button.svelte";
+    import Input from "./Input.svelte";
+
+</script>
+
+<aside class="main-sidebar">
+</aside>
+<section class="content-wrapper">
+    <nav class="nav-header">
+
+    </nav>
+</section> 
+
+<style>
+    .main-sidebar{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        transition: margin-left .3s ease-in-out,width .3s ease-in-out;
+        width: 250px;
+        height: 100vh;
+        background-color: #343A40;
+        box-shadow: 0 14px 28px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.22)!important;
+        z-index: 1038;
+    }
+    .content-wrapper{
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        height: 100vh;
+        background-color: #454D55;
+    }
+    .nav-header{
+        width: 100%;
+        height: 56px;
+        padding: 8px;
+        box-sizing: border-box;
+        background-color: #343A40;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
     let user = { loggedIn: false };
+	import Home from "../components/Home.svelte";
 	import Login from "../components/Login.svelte";
 </script>
 
 <main>
     {#if user.loggedIn}
-
+    <Home />
     {/if}
     {#if !user.loggedIn}
     <Login />
@@ -13,12 +14,12 @@
 </main>
 
 <style>
-    :global body{ background-color: #454D55; }
+    :global body{ background-color: #454D55; margin:0; padding:0; }
     main{
         display: flex;
         justify-content: center;
         align-items: center;
-        height: 95vh;
+        height: 100vh;
         font-family: 'Roboto', 'Helvetica', 'sans-serif';
     }
 </style>
