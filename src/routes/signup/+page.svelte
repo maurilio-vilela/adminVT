76b9cceb4d4d06da103879fa1df97cc14da8ea09
Maurilio Vilela <mauriliovilela20@gmail.com>
2022-12-22 11:39:20 -0300

    Create signup

---
+++ src/routes/signup/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    import LoginForm from "../login/+page.svelte";
+  </script>
+  
+  <div class="container">
+    <LoginForm title="Cadastrar" />
+  </div>
+  
+  <style>
+    .container {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      width: 100%;
+    }
+  </style>
