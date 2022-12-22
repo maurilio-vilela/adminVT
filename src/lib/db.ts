6f6ec9adda80b888f0eca7ddf6ebe8bcd1fd609e
Maurilio Vilela <mauriliovilela20@gmail.com>
2022-12-22 11:36:33 -0300

    Alterado para acesso direto aos dados do supabase

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
+  'https://dzthrfklifibqdtnawov.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6dGhyZmtsaWZpYnFkdG5hd292Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzA4MDM3OTYsImV4cCI6MTk4NjM3OTc5Nn0.Fr5gJU2Wz8szndrWtjHGD3bTl27XmUyPi4NbR1exxWQ'
 );
 
 export default supabase;
